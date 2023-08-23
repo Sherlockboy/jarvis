9dddb5ab1b2fa5487f4174837a2647b55eac539a
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2023-08-24 00:32:20 +0500

    config refactoring

--- packages/sherlockboy/mohirai/config/mohirai.php
+++ packages/sherlockboy/mohirai/config/mohirai.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'base_url' => env('MOHIRAI_URL', 'https://studio.mohir.ai/api/v1/stt'),
+    'base_url' => env('MOHIRAI_URL', 'https://studio.mohir.ai/api/v1'),
     'api_key' => env('MOHIRAI_KEY', '')
 ];
--- packages/sherlockboy/mohirai/src/MohiraiApi.php
+++ packages/sherlockboy/mohirai/src/MohiraiApi.php
@@ -9,10 +9,12 @@ class MohiraiApi
 {
     public static function stt(string $filePath)
     {
+        $baseUrl = config('mohirai.base_url');
+
         $response = Http::acceptJson()
             ->withHeader('Authorization', config('mohirai.api_key'))
             ->attach('file', Storage::readStream($filePath))
-            ->post(config('mohirai.base_url'));
+            ->post("$baseUrl/stt");
 
         return $response->json();
     }
